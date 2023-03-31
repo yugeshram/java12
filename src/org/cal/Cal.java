a5e099242469ff6f010c3ff65ba4bd096bd3a974
yugeshram <yugeshram1701@gmail.com>
2023-03-31 12:42:44 +0530

    ramm

---
+++ src/org/cal/Cal.java
@@ -0,0 +1,43 @@
+package org.cal;
+
+import java.util.Scanner;
+
+public class Cal {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
+
+
+
